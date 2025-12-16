9ff02003c51ff27bee3367fc099709611b4a7154
Isaac Snow <isaacjsnow@gmail.com>
2025-12-16 15:37:10 -0800

    hello

--- src/data/storage.ts
+++ src/data/storage.ts
@@ -1,13 +1,18 @@
 import { createServerFn } from "@tanstack/react-start";
 import { v4 as uuidv4 } from "uuid";
 import z from "zod";
-import { requireAuthenticated } from "@/auth/shared";
+import {
+	authMiddleware,
+	requireAuthenticated,
+	roleHasPermission,
+} from "@/auth/shared";
 import { rateLimitMiddleware } from "@/lib/rate-limits";
-import { internalServerError } from "@/lib/responses";
+import { forbidden, internalServerError } from "@/lib/responses";
 import { getSupabaseServerClient } from "@/supabase/server";
 
 export const getSignedUploadTokenFn = createServerFn()
 	.middleware([
+		authMiddleware,
 		requireAuthenticated,
 		// Limit example: allow 10 signed upload URLs per minute, with burst up to 5 at once.
 		rateLimitMiddleware({
@@ -18,31 +23,47 @@ export const getSignedUploadTokenFn = createServerFn()
 		}),
 	])
 	.inputValidator(
-		z.object({ bucket: z.string(), prefix: z.string(), filename: z.string() }),
+		z.object({
+			bucket: z.enum(["venues"]),
+			prefix: z.string(),
+			filename: z.string(),
+		}),
 	)
-	.handler(async ({ data: { bucket, prefix, filename } }) => {
-		const supabase = getSupabaseServerClient();
+	.handler(
+		async ({ context: { viewer }, data: { bucket, prefix, filename } }) => {
+			const hasPermission = viewer
+				? roleHasPermission(viewer?.role, {
+						[bucket]: ["update"],
+					})
+				: false;
 
-		try {
-			const timestamp = Date.now();
-			const uniqueId = uuidv4();
-			const extension = filename.split(".").pop();
-			const fileName = `${uniqueId}-${timestamp}.${extension}`;
-			const storagePath = `${prefix}/${fileName}`;
-
-			const { data, error } = await supabase.storage
-				.from(bucket)
-				.createSignedUploadUrl(storagePath);
-
-			if (error) {
-				throw internalServerError(error.message);
+			if (!hasPermission) {
+				throw forbidden();
 			}
 
-			return { token: data.token, storagePath };
-		} catch (error) {
-			throw internalServerError((error as Error).message);
-		}
-	});
+			const supabase = getSupabaseServerClient();
+
+			try {
+				const timestamp = Date.now();
+				const uniqueId = uuidv4();
+				const extension = filename.split(".").pop();
+				const fileName = `${uniqueId}-${timestamp}.${extension}`;
+				const storagePath = `${prefix}/${fileName}`;
+
+				const { data, error } = await supabase.storage
+					.from(bucket)
+					.createSignedUploadUrl(storagePath);
+
+				if (error) {
+					throw internalServerError(error.message);
+				}
+
+				return { token: data.token, storagePath };
+			} catch (error) {
+				throw internalServerError((error as Error).message);
+			}
+		},
+	);
 
 // export const deleteObject = createServerFn()
 // 	.middleware([
