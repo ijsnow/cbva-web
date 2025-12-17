d2094c870be2a4cff98d965409b4494038372af0
Isaac Snow <isaacjsnow@gmail.com>
2025-12-16 18:35:40 -0800

    hello

--- src/routes/api/tasks/cleanup/storage.ts
+++ src/routes/api/tasks/cleanup/storage.ts
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { db, type schema } from "@/db/connection";
+import type { Database } from "@/db/schema";
 import { internalServerError } from "@/lib/responses";
 import { getSupabaseServerClient } from "@/supabase/server";
 import { isNotNullOrUndefined } from "@/utils/types";
@@ -33,18 +35,57 @@ async function walk(
 	return paths;
 }
 
-const bucketLookupPaths: { [key: string]: string[] } = {
-	venues: ["headerImageSource", "thumbnailImageSource"],
+type TableNames = keyof typeof db.query;
+type Schema = NonNullable<typeof db._.schema>;
+
+type BucketLookupColumns = {
+	[bucket in TableNames]?: {
+		[column in keyof Schema[bucket]["columns"]]?: boolean;
+	};
+};
+
+const bucketLookupColumns: BucketLookupColumns = {
+	venues: { headerImageSource: true, thumbnailImageSource: true },
 };
 
 async function cleanupBucket(
 	supabase: ReturnType<typeof getSupabaseServerClient>,
-	bucketName: string,
+	bucketName: keyof Database["query"],
 	objects: { path: string; createdAt: string; updatedAt: string }[],
 ) {
-	const lookupPaths = bucketLookupPaths[bucketName];
+	const lookupColumns = bucketLookupColumns[bucketName];
 
-	// ...
+	// TODO: dynamically search through tables with the same name as bucketName and check to see if values
+	// of columns named in lookupColumns reference object paths in the objects list.
+
+	const storagePaths = objects.map(({ path }) => path);
+
+	const query = db.query[bucketName];
+
+	const result = await query.findMany({
+		columns: lookupColumns,
+		where: (t, { or, inArray }) => {
+			const filters = Object.keys(lookupColumns).map((key) =>
+				inArray(t[key], storagePaths),
+			);
+
+			return or(...filters);
+		},
+	});
+
+	const activePaths = new Set(result.flatMap((row) => Object.values(row)));
+
+	const toDelete = objects.filter(({ path }) => !activePaths.has(path));
+
+	console.log(
+		Array.from(activePaths),
+		toDelete,
+		objects.length,
+		"-",
+		toDelete.length,
+		"=",
+		objects.length - toDelete.length,
+	);
 }
 
 export const Route = createFileRoute("/api/tasks/cleanup/storage")({
@@ -66,7 +107,7 @@ export const Route = createFileRoute("/api/tasks/cleanup/storage")({
 				for (const bucket of result.data) {
 					const paths = await walk(supabase, bucket.name);
 
-					console.log(bucket.name, paths);
+					await cleanupBucket(supabase, bucket.name as TableNames, paths);
 				}
 
 				return new Response(JSON.stringify({ success: true }), {
