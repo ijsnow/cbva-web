0a7d1079de7add1202fae5eb4c7b189f75863886
Isaac Snow <isaacjsnow@gmail.com>
2026-01-09 18:07:35 -0800

    ok

---
+++ src/db/fix/director-users.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+
+import { eq } from "drizzle-orm";
+import { db } from "../connection";
+import { users } from "../schema";
+
+async function main() {
+	const directors = await db.query.directors.findMany({
+		with: {
+			profile: {
+				with: {
+					user: true,
+				},
+			},
+		},
+	});
+
+	const userIds = directors.map(({ profile: { userId } }) => userId);
+
+	await Promise.all(
+		userIds.map((userId) =>
+			db.update(users).set({ role: "td" }).where(eq(users.id, userId)),
+		),
+	);
+
+	process.exit(0);
+}
+
+await main();
